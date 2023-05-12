954b9863b4a9e1588d8a4a5eb7d5ffc4f6b1a5a8
VictorAkan <vikurekong123@gmail.com>
2023-05-12 17:00:20 -0400

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+// Node js basics
+const amount = 9
+
+if (amount < 10) {
+    console.log("small number")
+} else {
+    console.log("large number")
+}
+console.log("hey it's my first node app!!!")
